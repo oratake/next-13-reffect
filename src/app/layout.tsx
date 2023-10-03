eea18d25735ff2350f72653d887a336ad3398a83
oratake <smith.shimomura@gmail.com>
2023-10-03 16:55:05 +0900

    feat: Parallel Routesでanalyticsとteam要素をlayoutに指定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,12 +11,20 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  team,
+  analytics,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode,
+  team: React.ReactNode,
+  analytics: React.ReactNode,
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>{children}</div>
+        <div>{team}</div>
+        <div>{analytics}</div>
+      </body>
     </html>
   )
 }
