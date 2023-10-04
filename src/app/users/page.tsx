6bd6b138447cbfdf8bc4def20c8eae1788e31363
Kyosuke Takenaka <smith.shimomura@gmail.com>
2023-10-04 22:24:51 +0900

    feat: suspenseで一部分にロード画面を適用する

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import UserList from './UserList';
 
 const Page = async () => {
   return (
     <div className="m-4">
       <h1 className="text-lg font-bold">ユーザ一覧</h1>
-      <UserList />
+      <Suspense fallback={<p>Loading...</p>}>
+        <UserList />
+      </Suspense>
     </div>
   );
 };
