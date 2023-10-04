ad045126e3a44f7273b14abc5eaa16778c6330db
Kyosuke Takenaka <smith.shimomura@gmail.com>
2023-10-04 21:53:53 +0900

    feat: ユーザのリスト表示部分をコンポーネントにまとめる

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,28 +1,10 @@
-type User = {
-  id: string,
-  name: string,
-  email: string,
-};
-
-const getUsers = async (): Promise<User[]> => {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const users = response.json();
-  console.log(users);
-  return users;
-}
+import UserList from './UserList';
 
 const Page = async () => {
-  const users = await getUsers();
-
   return (
     <div className="m-4">
       <h1 className="text-lg font-bold">ユーザ一覧</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <UserList />
     </div>
   );
 };
