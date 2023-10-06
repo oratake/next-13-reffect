233148a80159028d16a1db5311dfcf428ac093f8
oratake <smith.shimomura@gmail.com>
2023-10-07 01:29:39 +0900

    feat: header,cookie取得

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,9 +1,13 @@
 import { NextResponse } from 'next/server';
+import { headers, cookies } from 'next/headers';
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const name = searchParams.get('name');
-  console.log(name);
+export async function GET() {
+  const headersList = headers();
+  const cookieStore = cookies();
+
+  console.log('headersList', headersList);
+  console.log('cookieStore', cookieStore);
+
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await response.json();
   return NextResponse.json(data);
