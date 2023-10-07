f7e7e2b3a81a7853ac709e798fdaae3c224568a1
Kyosuke Takenaka <smith.shimomura@gmail.com>
2023-10-07 17:15:52 +0900

    feat: APIエンドポイント経由でprismaからのデータを取得

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,13 +1,24 @@
 import prisma from '@/lib/prisma';
+import { Post } from '@prisma/client';
 
 const Page = async () => {
+  const response = await fetch('http://localhost:3000/api/posts');
+  const apiPosts: Post[] = await response.json();
+
   const posts = await prisma.post.findMany();
 
   return (
     <div className="m-4">
       <h1 className="text-lg font-bold">記事一覧</h1>
+      <h2>prisma直で取得</h2>
       <ul>
         {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+      <h2>APIから取得</h2>
+      <ul>
+        {apiPosts.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
       </ul>
