6d99082da5064375967e89081bfb1457475fa707
Kyosuke Takenaka <smith.shimomura@gmail.com>
2023-10-07 11:58:07 +0900

    feat: POSTのエンドポイント

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,14 +1,12 @@
 import { NextResponse } from 'next/server';
-import { headers, cookies } from 'next/headers';
 
 export async function GET() {
-  const headersList = headers();
-  const cookieStore = cookies();
-
-  console.log('headersList', headersList);
-  console.log('cookieStore', cookieStore);
-
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await response.json();
   return NextResponse.json(data);
+}
+
+export async function POST(request: Request) {
+  const res = await request.json();
+  return NextResponse.json({ res });
 }
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -3,6 +3,21 @@ import UserList from './UserList';
 import OtherUserList from './OtherUserList';
 
 const Page = async () => {
+  const response = await fetch('http://localhost:3000/api', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: 'John',
+      email: 'john@example.com',
+    }),
+  });
+
+  const data = await response.json();
+
+  console.log(data);
+
   return (
     <div className="m-4">
       <h1 className="text-lg font-bold">ユーザ一覧</h1>
